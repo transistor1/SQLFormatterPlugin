38fab0192d16807a5b253c10882d07678bc83d29
Steve Russo <svrusso1@gmail.com>
2015-11-02 23:25:12 -0500

    Initial Commit

---
+++ SQLFormatterOptions.cs
@@ -0,0 +1,134 @@
+﻿using MsaSQLEditor;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SQLFormatterPlugin
+{
+    /// <summary>
+    /// Your options class should be derived from IPluginOptions. Mark
+    /// the class with the [Serializable] attribute.
+    /// </summary>
+    [Serializable]
+    public class SQLFormatterOptions : IPluginOptions
+    {
+        Action SaveCallback = null;
+        Action CancelCallback = null;
+
+        /// <summary>
+        /// The DisplayName attribute gives an option a friendly name in the
+        /// SQL editor options, and the Category groups options together.
+        /// </summary>
+        [DisplayName("Trailing Commas"), Category("Options"), DefaultValue(false)]
+        public bool TrailingCommas { get; set; }
+
+        [DisplayName("Space After Comma"), Category("Options"), DefaultValue(false)]
+        public bool SpaceAfterComma { get; set; }
+
+        [DisplayName("Indent String"), Category("Options"), DefaultValue("\\t")]
+        public string IndentString { get; set; }
+
+        [DisplayName("Spaces Per Tab"), Category("Options"), DefaultValue(4)]
+        public int SpacesPerTab { get; set; }
+
+        [DisplayName("New Statement Line Breaks"), Category("Options"), DefaultValue(2)]
+        public int NewStatementLineBreaks { get; set; }
+
+        [DisplayName("New Clause Line Breaks"), Category("Options"), DefaultValue(1)]
+        public int NewClauseLineBreaks { get; set; }
+
+        [DisplayName("Max Line Width"), Category("Options"), DefaultValue(999)]
+        public int MaxLineWidth { get; set; }
+
+        [DisplayName("Expand Comma Lists"), Category("Options"), DefaultValue(true)]
+        public bool ExpandCommaLists { get; set; }
+
+        [DisplayName("Expand Boolean Expressions"), Category("Options"), DefaultValue(true)]
+        public bool ExpandBooleanExpressions { get; set; }
+
+        [DisplayName("Expand Case Statements"), Category("Options"), DefaultValue(true)]
+        public bool ExpandCaseStatements { get; set; }
+
+        [DisplayName("Expand Between Conditions"), Category("Options"), DefaultValue(true)]
+        public bool ExpandBetweenConditions { get; set; }
+
+        [DisplayName("Break Join On Sections"), Category("Options"), DefaultValue(false)]
+        public bool BreakJoinOnSections { get; set; }
+
+        [DisplayName("Uppercase Keywords"), Category("Options"), DefaultValue(true)]
+        public bool UppercaseKeywords { get; set; }
+
+        //[DisplayName("htmlcoloring"), Category("Options")]
+        //public bool HTMLColoring { get; set; }
+
+        [DisplayName("Keyword Standardization"), Category("Options"), DefaultValue(true)]
+        public bool KeywordStandardization { get; set; }
+
+        [DisplayName("Expand In Lists"), Category("Options"), DefaultValue(true)]
+        public bool ExpandInLists { get; set; }
+
+        public void Save()
+        {
+            if (SaveCallback != null)
+            {
+                this.SaveCallback();
+            }
+        }
+
+        public void Cancel()
+        {
+            if (this.CancelCallback != null)
+            {
+                this.CancelCallback();
+            }
+        }
+
+        public SQLFormatterOptions()
+        {
+            this.TrailingCommas = false;
+
+            this.SpaceAfterComma = false;
+
+            this.IndentString = "\\t";
+
+            this.SpacesPerTab = 4;
+
+            this.NewStatementLineBreaks = 2;
+
+            this.NewClauseLineBreaks = 1;
+
+            this.MaxLineWidth = 999;
+
+            this.ExpandCommaLists = true;
+
+            this.ExpandBooleanExpressions = true;
+
+            this.ExpandCaseStatements = true;
+
+            this.ExpandBetweenConditions = true;
+
+            this.BreakJoinOnSections = false;
+
+            this.UppercaseKeywords = true;
+
+            this.KeywordStandardization = true;
+
+            this.ExpandInLists = true;
+        }
+
+        public SQLFormatterOptions(Action saveCallback, Action cancelCallback) : this()
+        {
+            this.SetCallbacks(saveCallback, cancelCallback);
+        }
+
+        public void SetCallbacks(Action saveCallback, Action cancelCallback) 
+        {
+            this.SaveCallback = saveCallback;
+            this.CancelCallback = cancelCallback;
+        }
+    }
+}
---
+++ SQLFormatterPlugin.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Text;
+using MsaSQLEditor; //From MsaSQLEditor.dll
+using System.IO;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Configuration;
+using System.Xml.Serialization;
+
+namespace SQLFormatterPlugin
+{
+    /// <summary>
+    /// To create a plugin, add references in your project to MsaSQLEditor.dll
+    /// and ScintillaNET.dll, both of which are in your Access SQL Editor installation
+    /// folder.
+    /// </summary>
+    public class SQLFormatterPlugin : IPlugin
+    {
+        private SQLFormatterOptions _Options = null;
+
+        /// <summary>
+        /// Name is property of the IPlugin interface, and must be implemented.
+        /// Give your plugin a descriptive name.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return "Poor Man's T-SQL Formatter";
+            }
+        }
+
+        /// <summary>
+        /// ShortcutKeys is a property from IPlugin. Provide shortcut keys
+        /// for your plugin, or return null to only allow access through the
+        /// menu.
+        /// </summary>
+        public Keys ShortcutKeys
+        {
+            get
+            {
+                return Keys.Control | Keys.D1;
+            }
+        }
+
+        /// <summary>
+        /// Options is a property from IPlugin.  Note the return type is dynamic.
+        /// </summary>
+        public dynamic Options
+        {
+            get
+            {
+                return this._Options;
+            }
+        }
+
+        private void SaveSettings()
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(SQLFormatterOptions));
+            using (var memStream = new MemoryStream())
+            {
+                xmlSerializer.Serialize(memStream, _Options);
+                memStream.Position = 0;
+
+                using (var streamReader = new StreamReader(memStream))
+                {
+                    Properties.Settings.Default.SQLFormatterOptions = streamReader.ReadToEnd();
+                    Properties.Settings.Default.Save();
+                }
+            }
+        }
+
+        private void LoadSettings()
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(SQLFormatterOptions));
+            using (var memStream = new MemoryStream(Encoding.UTF8.GetBytes(Properties.Settings.Default.SQLFormatterOptions)))
+            {
+                try
+                {
+                    _Options = (SQLFormatterOptions)xmlSerializer.Deserialize(memStream);
+                    _Options.SetCallbacks(this.SaveSettings, this.LoadSettings);
+                }
+                catch { }
+            }
+        }
+
+        public SQLFormatterPlugin()
+        {
+            _Options = new SQLFormatterOptions(this.SaveSettings, this.LoadSettings);
+            LoadSettings();
+        }
+
+        /// <summary>
+        /// PerformAction is the action that is taken when your plugin is called.
+        /// </summary>
+        /// <param name="context">Currently, context only supplies a reference to the
+        /// ScintillaNET editor you're using, but in the future this will be 
+        /// expanded.</param>
+        public void PerformAction(IPluginContext context)
+        {
+            StringBuilder ExeArgs = new StringBuilder();
+
+            if (_Options.TrailingCommas)
+                ExeArgs.Append("--trailingcommas ");
+
+            if (_Options.SpaceAfterComma)
+                ExeArgs.Append("--spaceaftercomma ");
+
+            ExeArgs
+                .AppendFormat("--spacespertab {0} ", _Options.SpacesPerTab);
+            
+            ExeArgs
+                .AppendFormat("--indentstring \"{0}\" ", _Options.IndentString);
+
+            ExeArgs
+                .AppendFormat("--newstatementlinebreaks {0} ", _Options.NewStatementLineBreaks);
+            
+            ExeArgs
+                .AppendFormat("--newclauselinebreaks {0} ", _Options.NewClauseLineBreaks);
+
+            ExeArgs
+                .AppendFormat("--maxlinewidth {0} ", _Options.MaxLineWidth);
+
+            if (_Options.ExpandCommaLists)
+                ExeArgs.Append("--expandcommalists ");
+
+            if (_Options.ExpandBooleanExpressions)
+                ExeArgs.Append("--expandbooleanexpressions ");
+
+            if (_Options.ExpandCaseStatements)
+                ExeArgs.Append("--expandcasestatements ");
+
+            if (_Options.ExpandBetweenConditions)
+                ExeArgs.Append("--expandbetweenconditions ");
+            
+            if (_Options.BreakJoinOnSections)
+                ExeArgs.Append("--breakjoinonsections ");
+
+            if (_Options.UppercaseKeywords)
+                ExeArgs.Append("--uppercasekeywords ");
+
+            if (_Options.KeywordStandardization)
+                ExeArgs.Append("--keywordstandardization ");
+
+            if (_Options.ExpandInLists)
+                ExeArgs.Append("--expandinlists ");
+
+            ProcessStartInfo pi = new ProcessStartInfo(ExePath, ExeArgs.ToString());
+            pi.RedirectStandardInput = true;
+            pi.RedirectStandardOutput = true;
+            pi.RedirectStandardError = true;
+            pi.UseShellExecute = false;
+            pi.ErrorDialog = false;
+            pi.CreateNoWindow = true;
+
+            Process p = new Process();
+            p.StartInfo = pi;
+            p.Start();
+            
+            p.StandardInput.Write(Convert.ToBase64String(Encoding.UTF8.GetBytes(context.Editor.Text)));
+            p.StandardInput.Write((char)0x04);
+            
+            string Result = p.StandardOutput.ReadToEnd();
+            Result = Encoding.UTF8.GetString(Convert.FromBase64String(Result));
+            
+            p.WaitForExit();
+            context.Editor.Text = Result;
+        }
+
+        private string ExePath
+        {
+            get
+            {
+                return Path.Combine(
+                    Directory.GetParent(Assembly.GetExecutingAssembly().Location).FullName,
+                    "SQLFormatter.exe");
+            }
+        }
+    }
+}
